c26e16727b050904683f3f4633fad29e1886a125
Trista Runions <t.runions19@gmail.com>
2021-09-12 22:56:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from './Home'
+import Form from './Form'
 
 const App = () => {
   return (
-    <>
+    <div className="header">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <p>Your favorrite food, delivered while you code!!</p>
+      <nav>
+        
+        
+      <Link to="/"><button>Home</button></Link>
+      <Link to="/pizza"><button id="order-pizza">Order Pizza!</button></Link>
+       
+      </nav>
+  <Switch>
+      <Route path="/pizza" component={Form} />
+      <Route path="/" component={Home} />
+  </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+function Home(){
+    return(
+        
+        <div>
+            <p>Use the button above to start your order!</p>
+        </div>
+    )
+}
+
+export default Home
